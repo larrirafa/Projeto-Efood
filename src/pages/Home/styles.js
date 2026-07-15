93eb2c9a9b3b4e0dc78ca29ba24fafb6d35738a6
Larri <larrirafael2008@gmail.com>
2026-07-15 11:48:40 -0300

    Update background image in Hero section styles

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Hero = styled.section`
   background-color: ${({ theme }) => theme.colors.beigeLight};
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='60' height='60'%3E%3Ctext x='30' y='40' font-family='Arial, sans-serif' font-weight='700' font-size='26' fill='%23FFE4CE' text-anchor='middle'%3EH%3C/text%3E%3C/svg%3E");
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='60' height='60'%3E%3Cg transform='translate(22,10)' fill='none' stroke='%23FFE4CE' stroke-width='2.2' stroke-linecap='round'%3E%3Cline x1='0' y1='0' x2='0' y2='16'/%3E%3Cline x1='-4' y1='0' x2='-4' y2='8'/%3E%3Cline x1='4' y1='0' x2='4' y2='8'/%3E%3Cline x1='-4' y1='8' x2='4' y2='8'/%3E%3Cline x1='0' y1='16' x2='0' y2='40'/%3E%3Cpath d='M14 0c0 5-3 7-3 10s1 4 3 4 3-1 3-4-3-5-3-10z'/%3E%3Cline x1='14' y1='14' x2='14' y2='40'/%3E%3C/g%3E%3C/svg%3E");
   background-repeat: repeat;
   background-size: 60px 60px;
   padding: 64px 32px;
